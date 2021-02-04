e40492085237b98e0695d241228f869a16c1d36d
Ryan-F3107 <ryanfernandes3107@gmail.com>
2021-02-04 16:22:44 -0500

    :helicopter: added login & Signup Button on first page

--- App.tsx
+++ App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>First page of app -- App.tsx</Text>
+      <Button
+        title =  "Login"
+        onPress = {()=> Alert.alert("Button pressed")} 
+      />
+      <Button
+        title =  "Sign Up"
+        onPress = {()=> Alert.alert("Button pressed")} 
+      />
     </View>
   );
 }
