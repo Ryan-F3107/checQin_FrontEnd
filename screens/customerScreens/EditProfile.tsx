6fa7435338c88b1257f227688fe1f1d5ea0a1384
sooyeonch <48460686+sooyeonch@users.noreply.github.com>
2021-03-30 23:30:14 -0400

    updated a comment/message

--- screens/customerScreens/EditProfile.tsx
+++ screens/customerScreens/EditProfile.tsx
@@ -322,7 +322,7 @@ class EditProfile extends React.Component {
 												this.setState({ newEmail: '', newFirstName: '', newLastName: '', newPhoneNumber: '', contactPref: '' });
 												// Success Message
 												showMessage({
-													message: `Profile updated.`,
+													message: `Profile Updated.`,
 													type: "success",
 													autoHide: true,
 													duration: 2000,
